afe904fa622492d74d9fe49689284167baa8ab3f
sarink80 <sarah80nik@gmail.com>
2022-08-01 15:01:28 +0430

    the end

--- main.go
+++ main.go
@@ -4,17 +4,21 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/jung-kurt/gofpdf"
 )
 
 func main() {
 
 	arguments := os.Args
 
-	if len(arguments) == 1 {
-		fmt.Println("Usage  :  file1.txt   file2.txt  .. [filen.txt ... ]")
+	if len(arguments) == 2 {
+		fmt.Println("Usage  : Title  file1.txt   file2.txt  .. [filen.txt ... ]")
 	}
 
-	fileNames := arguments[1:]
+	title := arguments[1]
+
+	fileNames := arguments[2:]
 
 	var resultStr string
 
@@ -31,6 +35,25 @@ func main() {
 
 		resultStr += string(text)
 
+	}
+
+	// first argument is page orientation we can use P for portrait and L for landscape
+	pdf := gofpdf.New("P", "mm", "A5", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 14)
+	width, _ := pdf.GetPageSize()
+	pdf.MoveTo(width/2, 10)
+	pdf.Cell(1, 1, title)
+
+	pdf.MoveTo(0, 20)
+	pdf.SetFont("Arial", "", 14)
+
+	pdf.MultiCell(width, 10, resultStr, "", "", false)
+
+	err := pdf.OutputFileAndClose(title + ".pdf")
+
+	if err == nil {
+		fmt.Println("PDF generated successfully")
 	}
 
 }
