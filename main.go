b629a41e087eb0d4cd21888cf4353e70f4ed0705
sarink80 <sarah80nik@gmail.com>
2022-08-01 12:20:27 +0430

    Reading text files and converting them to a string  variable

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 func main() {
+
+	arguments := os.Args
+
+	if len(arguments) == 1 {
+		fmt.Println("Usage  :  file1.txt   file2.txt  .. [filen.txt ... ]")
+	}
+
+	fileNames := arguments[1:]
+
+	var resultStr string
+
+	for _, textfile := range fileNames {
+
+		text, err := ioutil.ReadFile(textfile)
+
+		if err != nil {
+
+			fmt.Println("An error occurred while reading the file " + textfile)
+
+			continue
+		}
+
+		resultStr += string(text)
+
+	}
 
 }
